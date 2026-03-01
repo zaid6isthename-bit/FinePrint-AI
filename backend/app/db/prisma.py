bcbf98d4a0ed10c8a8083b0e50ccac9df88ad36c
zaid6isthename-bit <zaid6isthename@gmail.com>
2026-03-01 17:50:41 +0530

    Remove prisma auto_register causing startup freeze

--- backend/app/db/prisma.py
+++ backend/app/db/prisma.py
@@ -1,11 +1,4 @@
 from prisma import Prisma
-import logging
-
-logger = logging.getLogger(__name__)
-
-db = Prisma(auto_register=True)
-
-from prisma import Prisma
 import asyncio
 
 prisma = Prisma()
@@ -21,10 +14,6 @@ async def connect_db():
             await asyncio.sleep(2)
 
 async def disconnect_db():
-    try:
-        if db.is_connected():
-            await db.disconnect()
-            logger.info("Successfully disconnected from the database.")
-    except Exception as e:
-        logger.error(f"Error disconnecting from database: {e}")
-        raise e
+    if prisma.is_connected():
+        await prisma.disconnect()
+        print("Disconnected from database")
